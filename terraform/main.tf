49547e16b23162100ddfbdcfba7604a560539e64
SuhodolovaO <olsuhodolova@gmail.com>
2021-12-06 21:02:32 +0500

    Switched to Yandex Cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,48 +1,37 @@
-provider "aws" {
-   region = "eu-north-1"
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+    }
+  }
 }
 
-data "aws_ami" "amazon_linux" {
-	most_recent = true
-	owners      = ["amazon"]
-	filter {
-		name = "name"
-		values = ["amzn-ami-hvm-*-x86_64-gp2"]
-	}
-	filter {
-		name = "owner-alias"
-		values = ["amazon"] 
-	}
+provider "yandex" {
+  cloud_id  = "b1g8rpon0a070cte64jt"
+  folder_id = "b1giq47cia0l9tim5cpl"
+  zone="ru-central1-b"
 }
 
-locals {
-	netology_instance_types = {
-		stage = "t3.micro"
-		prod = "t3.large"
-	}
-	netology_instance_count = {
-		stage = 1
-		prod = 2
-	}
-	netology2_instances = {
-		"t3.micro" = data.aws_ami.amazon_linux.id   
-		"t3.large" = data.aws_ami.amazon_linux.id
-	}
+data "yandex_compute_image" "coi" {
+  family = "container-optimized-image"
 }
 
-resource "aws_instance" "netology" {
-	ami = data.aws_ami.amazon_linux.id
-	instance_type = local.netology_instance_types[terraform.workspace]
-	count = local.netology_instance_count[terraform.workspace]
-	
-	lifecycle {
-		create_before_destroy = true
-	}
-}
-
-resource "aws_instance" "netology-2" {
-	for_each = local.netology2_instances
-	
-	ami = each.value
-	instance_type = each.key
+resource "yandex_compute_instance" "instance-coi" {
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.coi.id
+    }
+  }
+  network_interface {
+    subnet_id = "e2lep8quibfestinq1rk"
+    nat = true
+  }
+  resources {
+    cores = 2
+    memory = 2
+  }
+  metadata = {
+    docker-container-declaration = file("${path.module}/declaration.yaml")
+    user-data = file("${path.module}/cloud_config.yaml")
+  }
 }
